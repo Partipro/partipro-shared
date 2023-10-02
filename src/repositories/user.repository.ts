56763bae7ed51a4f2adb0871016774b37f281857
gabrieltakaki <gabitj@icloud.com>
2023-10-01 22:04:48 -0300

    feat: repository and service for the user

---
+++ src/repositories/user.repository.ts
@@ -0,0 +1,12 @@
+import { Model } from "mongoose";
+import BaseRepository from "../core/base.repository";
+import User from "../models/user/user.model";
+import { IUser } from "../models/user/user.interface";
+
+class UserRepository extends BaseRepository<IUser, Model<IUser>> {
+  constructor() {
+    super(User);
+  }
+}
+
+export default new UserRepository();
