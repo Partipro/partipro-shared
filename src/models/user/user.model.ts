2e490198b036be10aa2e8e0bb6670febee9d0f81
gabrieltakaki <gabitj@icloud.com>
2023-10-04 20:58:33 -0300

    feat: hashes user password

--- src/models/user/user.model.ts
+++ src/models/user/user.model.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import { IUser } from "./user.interface";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema<IUser>({
   name: {
@@ -20,6 +21,32 @@ const userSchema = new Schema<IUser>({
     type: Schema.Types.ObjectId,
   },
 });
+
+userSchema.pre("save", (next) => {
+  const user: unknown = this;
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+    bcrypt.hash((user as IUser).password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      (user as IUser).password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = (password: string, cb: (param1: Error | null, param2?: boolean) => void) => {
+  const user: unknown = this;
+  bcrypt.compare(password, (user as IUser).password, (err, isMatch) => {
+    if (err) {
+      return cb(err);
+    }
+    cb(null, isMatch);
+  });
+};
 
 const User = model<IUser>("User", userSchema);
 
