65a619277911f480b52c7f76c7e05f8e93ffae6a
gabrieltakaki <gabitj@icloud.com>
2023-10-05 14:54:05 -0300

    fix: pre save user

--- src/models/user/user.model.ts
+++ src/models/user/user.model.ts
@@ -22,25 +22,25 @@ const userSchema = new Schema<IUser>({
   },
 });
 
-userSchema.pre("save", (next) => {
-  const user: unknown = this;
+userSchema.pre("save", function (next) {
+  const user = this as IUser;
   bcrypt.genSalt(10, (err, salt) => {
     if (err) {
       return next(err);
     }
-    bcrypt.hash((user as IUser).password, salt, (err, hash) => {
+    bcrypt.hash(user.password, salt, (err, hash) => {
       if (err) {
         return next(err);
       }
-      (user as IUser).password = hash;
+      user.password = hash;
       next();
     });
   });
 });
 
-userSchema.methods.comparePassword = (password: string, cb: (param1: Error | null, param2?: boolean) => void) => {
-  const user: unknown = this;
-  bcrypt.compare(password, (user as IUser).password, (err, isMatch) => {
+userSchema.methods.comparePassword = function (password: string, cb: (param1: Error | null, param2?: boolean) => void) {
+  const user = this as IUser;
+  bcrypt.compare(password, user.password, (err, isMatch) => {
     if (err) {
       return cb(err);
     }
