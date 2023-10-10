33a2c9934e9607accdca9dd14923f82c175239ba
gabrieltakaki <gabitj@icloud.com>
2023-10-09 22:01:47 -0300

    fix: id type

--- src/middlewares/Auth.ts
+++ src/middlewares/Auth.ts
@@ -15,7 +15,7 @@ export default (req: Request, res: Response, next: NextFunction) => {
 
     req.user = jwt.verify(token, SECRET_KEY) as {
       email: string;
-      id: number;
+      id: string;
     };
     next();
   } catch (err) {
--- src/types/express/index.d.ts
+++ src/types/express/index.d.ts
@@ -2,7 +2,7 @@ declare namespace Express {
   export interface Request {
     user: {
       email: string;
-      id: number;
+      id: string;
     };
   }
 }
