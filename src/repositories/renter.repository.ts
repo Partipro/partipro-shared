777bffe4782cdd4237ec06e0a7bef586bd43a85d
gabrieltakaki <gabitj@icloud.com>
2023-10-28 19:09:40 -0300

    fix: remove unused imports

--- src/repositories/renter.repository.ts
+++ src/repositories/renter.repository.ts
@@ -1,7 +1,5 @@
 import { Model } from "mongoose";
 import BaseRepository from "../core/base.repository";
-import { IProperty } from "../models/property/property.interface";
-import Property from "../models/property/property.model";
 import { IRenter } from "../models/renter/renter.interface";
 import Renter from "../models/renter/renter.model";
 
