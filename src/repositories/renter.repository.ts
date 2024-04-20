d6ee493ab17f9a820c0952ff2cd93ef8d6e555b1
gabrieltakaki <gabitj@icloud.com>
2024-04-20 17:30:51 -0300

    refactor: general renters feature details

--- src/repositories/renter.repository.ts
+++ src/repositories/renter.repository.ts
@@ -1,10 +1,15 @@
 import BaseRepository from "../core/base.repository";
 import User from "../models/user/user.model";
-import { IUser } from "../models/user/user.interface";
+import { IUser, Roles } from "../models/user/user.interface";
+import { Find } from "../core/repository";
 
 class RenterRepository extends BaseRepository<IUser> {
   constructor() {
     super(User);
+  }
+
+  list({ filters, populate, sort, select }: Find<IUser> = {}): Promise<IUser[]> {
+    return super.list({ filters: { ...filters, role: Roles.RENTER }, populate, sort, select });
   }
 }
 
